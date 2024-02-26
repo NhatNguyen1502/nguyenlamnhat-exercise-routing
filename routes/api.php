30c3a0a1d84f162c97aa141bf984f0dd4db06b53
nguyenlamnhat <zznhatphilong@gmail.com>
2024-02-26 15:31:09 +0700

    done

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,47 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('user')->group(function () {
+    Route::get('/', function(){
+        global $users;
+        return $users; 
+    });
+    
+    Route::get('/{userIndex}', function ($userIndex) {
+        global $users;
+        if (array_key_exists($userIndex, $users)) {
+            return $users[$userIndex];
+        } else {
+            return 'You cannot get a user like this!';
+        }
+    }) -> where('userIndex', '[0-9]+');
+    
+    Route::get('/{userName}', function ($userName) {
+        global $users;
+        foreach($users as $user ) {
+            if ($user['name'] == $userName) return $user; 
+        }
+        return "Canot find the user with name ". $userName;
+    }) -> where('userName', '[a-zA-Z]+');
+
+    Route::get('/{userIndex}/post/{postIndex}', function ($userIndex, $postIndex) {
+        global $users;
+        if (array_key_exists($userIndex, $users)) {
+            $user = $users[$userIndex];
+            if (array_key_exists($postIndex, $user['posts'])) {
+                return $user['posts'][$postIndex];
+            } else {
+                return 'Cannot find the post with id ' . $postIndex . ' for user ' . $userIndex;
+            }
+        } else {
+            return $users[$userIndex];
+        }
+    });
+
+    Route::fallback(function () {
+        return 'You cannot get a user like this !';
+    });
+
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function(){
+    global $users;
+    return view('user', ["users" => $users]);
+});
